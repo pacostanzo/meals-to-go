2ae1120284fb80d6aa7d272f1def66c6784a8ccc
Pablo Costanzo <costanzopa@gmail.com>
2022-08-08 22:19:01 -0300

    Checkpoint map region

--- src/features/map/screens/Map/index.jsx
+++ src/features/map/screens/Map/index.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+
+import { LocationContext } from '../../../../services/location/location.context';
+import { RestaurantsContext } from '../../../../services/restaurants/restaurants.context';
 
 import { Search } from '../../components/Search';
 import { Map } from './Map.styles';
 
-export const MapScreen = () => (
-  <>
-    <Search />
-    <Map />
-  </>
-);
+export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const [latDelta, setLatDelta] = useState(0);
+
+  const { lat, lng, viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
+  return (
+    <>
+      <Search />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return null;
+        })}
+      </Map>
+    </>
+  );
+};
