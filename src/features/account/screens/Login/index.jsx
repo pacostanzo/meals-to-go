69b57c8290c8eecbc403d11da02f312b76939e1b
Pablo Costanzo <costanzopa@gmail.com>
2022-08-27 12:41:08 -0300

    Adding Login Screen with errors.

--- src/features/account/screens/Login/index.jsx
+++ src/features/account/screens/Login/index.jsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 
-import { AccountBackground } from '../../components/Account.styles';
+import { Spacer } from '../../../../components/Spacer';
+import { Text } from '../../../../components/Text';
+import { AuthenticationContext } from '../../../../services/authentication/authentication.context';
+
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+  AuthInput,
+} from '../../components/Account.styles';
 
 export const LoginScreen = () => {
-  return <AccountBackground />;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { onLogin, error } = useContext(AuthenticationContext);
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <AccountContainer>
+        <AuthInput
+          label="E-mail"
+          value={email}
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={(u) => setEmail(u)}
+        />
+        <Spacer size="large">
+          <AuthInput
+            label="Password"
+            value={password}
+            textContentType="password"
+            secureTextEntry
+            autoCapitalize="none"
+            secure
+            onChangeText={(p) => setPassword(p)}
+          />
+        </Spacer>
+        {error && (
+          <Spacer size="large">
+            <Text variant="error">{error}</Text>
+          </Spacer>
+        )}
+        <Spacer size="large">
+          <AuthButton
+            icon="lock-open-outline"
+            mode="contained"
+            onPress={() => onLogin(email, password)}
+          >
+            Login
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
+    </AccountBackground>
+  );
 };
--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -17,7 +17,7 @@ export const AuthenticationContextProvider = ({ children }) => {
       })
       .catch((e) => {
         setIsLoading(false);
-        setError(e);
+        setError(e.toString());
       });
   };
 
