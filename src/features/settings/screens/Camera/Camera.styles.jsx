4734c7ce5581960c3423be67689fc729c7cc277b
Pablo Costanzo <costanzopa@gmail.com>
2022-08-31 20:03:24 -0300

    Adding camera screen

---
+++ src/features/settings/screens/Camera/Camera.styles.jsx
@@ -0,0 +1,14 @@
+import { Camera } from 'expo-camera';
+import styled from 'styled-components/native';
+
+export const ProfileCamera = styled(Camera)`
+  width: 100%;
+  height: 100%;
+  flex: 1;
+`;
+
+export const InnerSnap = styled.View`
+  width: 100%;
+  height: 100%;
+  z-index: 999;
+`;
--- src/features/settings/screens/Camera/index.jsx
+++ src/features/settings/screens/Camera/index.jsx
@@ -1,3 +1,44 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import { Camera } from 'expo-camera';
 
-export const CameraScreen = () => null;
+import { View, TouchableOpacity } from 'react-native';
+import { Text } from '../../../../components/Typography';
+
+import { ProfileCamera, InnerSnap } from './Camera.styles';
+
+export const CameraScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const cameraRef = useRef();
+
+  const snap = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      console.log(photo);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <ProfileCamera
+      ref={(camera) => (cameraRef.current = camera)}
+      type={Camera.Constants.Type.front}
+    >
+      <TouchableOpacity onPress={snap}>
+        <InnerSnap />
+      </TouchableOpacity>
+    </ProfileCamera>
+  );
+};
